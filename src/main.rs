b1b60c8ec0e81a9a6e84783387ca2194edf0c19f
Anton Kaiukov <batkowm@gmail.com>
2022-07-26 22:19:11 -0700

    Update by lines, constant speed, Ctrl-C intercept

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ pub mod functions;
 pub mod vec2;
 pub mod vec3;
 use functions::*;
-use std::time::Instant;
+use std::{time::Instant, process::exit};
 use vec2::Vec2;
 use vec3::Vec3;
 extern crate console;
@@ -19,25 +19,38 @@ use crossterm::{
 fn main() {
     // let mut width = 120usize;
     // let mut height = 30usize;
-    let mut out = stdout();
+    _ = queue!(stdout(), Hide, EnterAlternateScreen);
+    ctrlc::set_handler(move || {
+        println!("received Ctrl+C!");
+        _ = execute!(stdout(), Show, LeaveAlternateScreen);
+        exit(0);
+    })
+    .expect("Error setting Ctrl-C handler");
+
     let (height, width) = Term::buffered_stdout().size();
     let (height, width) = (height as usize, width as usize);
-    queue!(out, Hide, EnterAlternateScreen);
     let aspect = width as f64/ height as f64;
     let pixel_aspect = 11.0 / 24.0;
-    let gradient = " .:;!/|({$&%@".as_bytes();
+    let gradient = " .:;!/|({%@$&".as_bytes();
+    // let gradient = " .'`,-^\"_:;!><i?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$".as_bytes();
     let gradient_size = gradient.len() - 1;
-    let mut screen = vec![' ' as u8; width * height];
-    for t in 0..10000 {
-        let ts = Instant::now();
+    let mut screen = vec![vec![' ' as u8; width]; height];
+    let ts_start = Instant::now();
+    let sphere1_pos = Vec3::new((0.0, 3.0, 0.0));
+    let sphere2_pos = Vec3::new((3.0, 0.0, 0.0));
+    let sphere3_pos = Vec3::new((0.0, -3.0, 0.0));
+    let sphere4_pos = Vec3::new((-3.0, 0.0, 0.0));
+    let cube_pos = Vec3::new((0.0, 0.0, -1.0));
+    loop {
         // Main loop
+        let ts = Instant::now();
+        let t = ts.saturating_duration_since(ts_start).as_millis()/10;
         let light = Vec3::new((-0.5, 0.5, -1.0)).norm();
-        let sphere_pos = Vec3::new((0.0, 3.0, 0.0));
         for j in 0..height {
             for i in 0..width {
                 let mut uv = Vec2::new((i, j)) / Vec2::new((width, height)) * 2.0 - 1.0;
                 uv.x *= aspect * pixel_aspect;
-                let mut ro = Vec3::new((-6.0, 0.0, 0.0));
+                let mut ro = Vec3::new((-10.0, 0.0, 0.0));
                 let mut rd = Vec3::new((2.0, uv)).norm();
                 ro = rotate_y(ro, 0.25);
                 rd = rotate_y(rd, 0.25);
@@ -46,25 +59,14 @@ fn main() {
                 let mut diff = 1.0;
                 for _k in 0..5 {
                     let mut min_it = 99999.0;
-                    let intersection = sphere(ro - sphere_pos, rd, 1.0);
                     let mut n = Vec3::new(0.0);
                     let mut albedo = 1.0;
-                    if intersection.x > 0.0 {
-                        let it_point = ro - sphere_pos + rd * intersection.x;
-                        min_it = intersection.x;
-                        n = it_point.norm();
-                    }
-                    let (intersection, cube_n) = cube(ro, rd, Vec3::new(1.0));
-                    if intersection.x > 0.0 && intersection.x < min_it {
-                        min_it = intersection.x;
-                        n = cube_n;
-                    }
-                    let intersection = Vec2::new(plane(ro, rd, Vec3::new((0.0, 0.0, -1.0)), 1.0));
-                    if intersection.x > 0.0 && intersection.x < min_it {
-                        min_it = intersection.x;
-                        n = Vec3::new((0.0, 0.0, -1.0));
-                        albedo = 0.5;
-                    }
+                    intersect_sphere(ro, rd, sphere1_pos, 1.0, &mut min_it, &mut n);
+                    intersect_sphere(ro, rd, sphere2_pos, 1.0, &mut min_it, &mut n);
+                    intersect_sphere(ro, rd, sphere3_pos, 1.0, &mut min_it, &mut n);
+                    intersect_sphere(ro, rd, sphere4_pos, 1.0, &mut min_it, &mut n);
+                    intersec_cube(ro, rd, cube_pos, &mut min_it, &mut n);
+                    intersec_plane(ro, rd, Vec3::new(1.0), &mut min_it, &mut n, &mut albedo);
                     if min_it < 99999.0 {
                         diff *= (n.dot(light) * 0.5 + 0.5) * albedo;
                         ro = ro + rd * (min_it - 0.01);
@@ -76,16 +78,40 @@ fn main() {
                 let mut color = (diff * 20.0) as usize;
                 color = color.clamp(0,gradient_size);
                 let pixel = gradient[color];
-                if screen[i+j*width] != pixel {
-                    screen[i+j*width] = pixel;
-                    execute!(out, MoveTo(i  as u16, j as u16), Print(String::from_utf8_lossy(&[pixel])));
-                }
+                screen[j][i] = pixel;
+                
             }
+            _ = execute!(stdout(), MoveTo(0, j as u16), Print(String::from_utf8_lossy(&screen[j])));
             // screen[(j+1) * width] = '\n' as u8;
         }
         // Get FPS
-        let ts_new = Instant::now();
-        queue!(out, MoveTo(0, height as u16), Print("FPS: "), Print(1000000 / ts_new.saturating_duration_since(ts).as_micros()), Print(" "));
+        let ts_new = Instant::now();        
+        _ = queue!(stdout(), MoveTo(0, height as u16), Print("FPS: "), Print(1000000 / ts_new.saturating_duration_since(ts).as_micros()), Print(" "));
     }
-    queue!(out, Show, LeaveAlternateScreen);
+}
+
+fn intersec_plane(ro: Vec3, rd: Vec3, pos: Vec3, min_it: &mut f64, n: &mut Vec3, albedo: &mut f64) {
+    let intersection = Vec2::new(plane(ro - pos, rd, Vec3::new((0.0, 0.0, -1.0)), 1.0));
+    if intersection.x > 0.0 && intersection.x < *min_it {
+        *min_it = intersection.x;
+        *n = Vec3::new((0.0, 0.0, -1.0));
+        *albedo = 0.2;
+    }
+}
+
+fn intersec_cube(ro: Vec3, rd: Vec3, pos: Vec3, min_it: &mut f64, n: &mut Vec3) {
+    let (intersection, cube_n) = cube(ro - pos, rd, Vec3::new(1.0));
+    if intersection.x > 0.0 && intersection.x < *min_it {
+        *min_it = intersection.x;
+        *n = cube_n;
+    }
+}
+
+fn intersect_sphere(ro: Vec3, rd: Vec3, pos: Vec3, r: f64, min_it: &mut f64, n: &mut Vec3) {
+    let intersection = sphere(ro - pos, rd, r);
+    if intersection.x > 0.0 && intersection.x < *min_it {
+        let it_point = ro - pos + rd * intersection.x;
+        *min_it = intersection.x;
+        *n = it_point.norm();
+    }
 }
